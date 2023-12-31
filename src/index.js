7948789ec2b0f48b964f1ec409d7fe9ad7f97a4e
Arthur Mariano <90999445+arthvm@users.noreply.github.com>
2023-12-31 14:36:39 -0300

    Clean index.js code
    This commit creates a new file: handleUserInput, that contains functions
    to get the user Input for the TODO Attributes. This allows the index.js
    file to handle only the TODO actions(like setting attributes, creating
    and deleting items), therefore cleaning the code.

--- src/index.js
+++ src/index.js
@@ -1,34 +1,25 @@
 import _, { create } from "lodash";
 import todoItem from "./todoClass.js";
-
-const getTitle = function () {
-  let title = prompt("TODO's title:", "New TODO");
-  return title;
-};
-
-const getDescription = function () {
-  let description = prompt("TODO's description:", "...");
-  return description;
-};
-
-const getDueDate = function () {
-  let dueDate = prompt("Due Date:", "mm/dd/yyyy");
-  return dueDate;
-};
-
-const getPriority = function () {
-  let priority = prompt("TODO's priority:", "Low/Medium/High");
-  return priority;
-};
+import {
+  getTitle,
+  getDescription,
+  getDueDate,
+  getPriority,
+} from "./handleUserInput.js";
 
 const createTodo = function () {
   let newTodo = new todoItem();
-  newTodo.title = getTitle();
-  newTodo.description = getDescription();
-  newTodo.dueDate = getDueDate();
-  newTodo.priority = getPriority();
-
   return newTodo;
 };
 
-console.table(createTodo());
+const setTodoAttributes = function (currentTodo) {
+  currentTodo.title = getTitle();
+  currentTodo.description = getDescription();
+  currentTodo.dueDate = getDueDate();
+  currentTodo.priority = getPriority();
+
+  return currentTodo;
+};
+
+//TEST
+console.table(setTodoAttributes(createTodo()));
